ca44399b9539ae9cb58c4541f4779e0a59cfb548
amel fradj <amelfradj1@gmail.com>
2023-10-11 13:57:37 +0200

    ok

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ function french(){
     document.getElementById("title").innerHTML="bonjour";
     document.getElementById("paragra").innerHTML="texte de mon pragrapghe";
 }
-
+function sombre(){
+    document.getElementById("fond").style.backgroundColor="black";
+    document.getElementById("fond").style.color="white";
+}
+function clair(){
+    document.getElementById("fond").style.backgroundColor="white";
+    document.getElementById("fond").style.color="black";
+}
 document.getElementById("en").addEventListener("click",anglais)
 document.getElementById("fr").addEventListener("click",french)
+document.getElementById("noir").addEventListener("click",sombre)
+document.getElementById("clairs").addEventListener("click",clair)
